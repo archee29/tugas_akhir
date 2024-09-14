ec2a4446c30bd4053c37a576af7a6f52df99a1f0
trioocta <67048687+TrioOctavianSyah@users.noreply.github.com>
2024-09-15 02:47:40 +0700

    fixing reset password backend

--- lib/app/modules/reset/controllers/reset_controller.dart
+++ lib/app/modules/reset/controllers/reset_controller.dart
@@ -7,20 +7,24 @@ import './../../../../app/widgets/dialog/custom_notification.dart';
 class ResetController extends GetxController {
   RxBool isLoading = false.obs;
   TextEditingController emailController = TextEditingController();
-
   FirebaseAuth auth = FirebaseAuth.instance;
 
   Future<void> sendEmail() async {
     if (emailController.text.isNotEmpty) {
       isLoading.value = true;
       try {
-        auth.sendPasswordResetEmail(email: emailController.text);
+        await auth.sendPasswordResetEmail(email: emailController.text);
         Get.back();
         CustomNotification.successNotification("Sukses",
             "Kami Telah mengirimkan link untuk ubah Password Ke Email Anda");
       } catch (e) {
-        CustomNotification.errorNotification("Terjadi Kesalahan",
-            "Tidak Dapat Mengirimkan Link Untuk Ubah Password Karena : ${e.toString()}");
+        if (e is FirebaseAuthException && e.code == 'user-not-found') {
+          CustomNotification.errorNotification("Terjadi Kesalahan",
+              "Email tidak terdaftar. Silakan periksa kembali email Anda.");
+        } else {
+          CustomNotification.errorNotification("Terjadi Kesalahan",
+              "Tidak Dapat Mengirimkan Link Untuk Ubah Password Karena : ${e.toString()}");
+        }
       } finally {
         isLoading.value = false;
       }
